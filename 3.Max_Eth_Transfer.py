6704bd6b34a019b5716551072b84c73f7e58c011
Plaz3rr <159921477+Plaz3rr@users.noreply.github.com>
2024-02-15 01:57:56 +0300

    Update 3.Max_Eth_Transfer.py

--- 3.Max_Eth_Transfer.py
+++ 3.Max_Eth_Transfer.py
@@ -1,7 +1,9 @@
 from web3 import Web3
 import time
+import os
+import sys
 
-#Проверь ГАЗ! 27 строка
+#Проверь ГАЗ! 50 строка
 
 #Скрипт отправляет максимально возможное количество ETH с кошельков senders.txt на кошельки(напр. суббакаунты) receivers.txt
 #Выжидает нужный газ и отправляет, в случае ошибки через 60сек повторяет всё заново с кошелька на котором остановился
@@ -16,6 +18,27 @@ def read_receivers(receivers_file):
         receivers = [line.strip().lower() for line in rcv_file.readlines()]  # Приведение адресов к нижнему регистру
     return receivers
 
+# Файл для сохранения информации о последней успешной транзакции
+last_tx_file = 'last_successful_tx3.txt'
+
+# Функция для чтения последней успешной транзакции из файла
+def read_last_successful_tx():
+    if os.path.exists(last_tx_file):
+        with open(last_tx_file, 'r') as file:
+            return file.read()
+    return None
+
+
+# Функция для сохранения последней успешной транзакции в файл
+def save_last_successful_tx(account_address, receiver_address, amount_eth, tx_hash):
+    tx_info = f"От {account_address} >>> {amount_eth}ETH на {receiver_address}, хеш: {tx_hash}"
+    with open(last_tx_file, 'w') as file:
+        file.write(tx_info)
+
+
+#читаем информацию о последней успешной транзакции
+last_successful_tx = read_last_successful_tx()
+
 # Функция для считывания приватных ключей и адресов получателей
 def read_private_keys_and_receivers(private_keys_file, receivers_file):
     with open(private_keys_file, 'r') as pk_file:
@@ -24,7 +47,8 @@ def read_private_keys_and_receivers(private_keys_file, receivers_file):
         receivers = [line.strip() for line in rcv_file.readlines()]
     return private_keys, receivers
 
-def send_max_eth_if_gas_is_low(private_key, receiver, max_gas_price_gwei=31):
+def send_max_eth_if_gas_is_low(private_key, receiver, max_gas_price_gwei=18):
+    global last_successful_tx  # Используем глобальную переменную для хранения данных о последней успешной транзакции
     receiver = web3.toChecksumAddress(receiver)
     account = web3.eth.account.privateKeyToAccount(private_key)
 
@@ -62,6 +86,12 @@ def send_max_eth_if_gas_is_low(private_key, receiver, max_gas_price_gwei=31):
         signed_tx = web3.eth.account.sign_transaction(tx, private_key)
         tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
         print(f"\033[92m>>>От {account.address} >>> {web3.fromWei(amount_to_send, 'ether')}ETH на {receiver}, хеш: {web3.toHex(tx_hash)}\033[0m")
+        
+        # Здесь сохраняем информацию о последней успешной транзакции
+        
+        amount_eth = web3.fromWei(amount_to_send, 'ether')
+        save_last_successful_tx(account.address, receiver, amount_eth, web3.toHex(tx_hash))
+        
         break  # Выходим из цикла после успешной отправки
 
 try:
@@ -75,5 +105,9 @@ try:
         send_max_eth_if_gas_is_low(private_key, receiver)
 
 except KeyboardInterrupt:
-    # Выводим сообщение о остановке скрипта зелёным цветом
-    print('\033[92m>>>Скрипт остановлен пользователем.\033[0m')
+            if last_successful_tx:
+                print('\033[92m' + ">>>Скрипт остановлен. Последняя успешная транзакция:" + '\033[0m')
+                print('\033[92m' + last_successful_tx + '\033[0m')
+            else:
+                print('\033[92m' + ">>>Скрипт остановлен. Нет данных о последней успешной транзакции." + '\033[0m')
+            sys.exit()
